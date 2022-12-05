c1936ef6b4b7a1e9a2f8a52e709e4f2a16a80720
L-ynx <raza.gh97@gmail.com>
2022-12-05 23:24:26 +0100

    Properly close socket and add comments

--- src/twmailer-client.cpp
+++ src/twmailer-client.cpp
@@ -7,9 +7,164 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+static void usage();
+static void commands();
+static void help();
 std::string sendMsg();
 std::string listMsg();
 std::string readOrDelMsg();
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in server;
+    int port;
+    char buffer[4096];
+
+    if (argc != 3) {
+        usage();
+        exit(1);
+    }
+
+    // Create a client socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket error.");
+        return EXIT_FAILURE;
+    }
+
+    port = atoi(argv[2]);
+
+    // Assign protocol family, port and address of server
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    inet_aton(argv[1], &server.sin_addr);
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == -1) {
+        perror("Connect error - no server available");
+        return EXIT_FAILURE;
+    }
+
+    // Print available commands
+    commands();
+
+    std::string input;
+    // Send and receive data
+    do {
+        bool validCmd = false;
+        std::string data;
+
+        // Enter valid command
+        while (!validCmd) {
+            data = "";
+            std::cout << "> ";
+            getline(std::cin, input);
+            data += input;
+
+            if (strcasecmp(data.c_str(), "QUIT") == 0) {
+                if (sock != -1) {
+                    if (shutdown(sock, SHUT_RDWR) == -1) {
+                        perror("shutdown sock");
+                    }
+                    if (close(sock) == -1) {
+                        perror("close sock");
+                    }
+                    sock = -1;
+                }
+                return EXIT_SUCCESS;
+            }
+
+            if (strcasecmp(data.c_str(), "SEND") == 0) {
+                data += sendMsg();
+                validCmd = true;
+            } else if (strcasecmp(data.c_str(), "LIST") == 0) {
+                data += listMsg();
+                validCmd = true;
+            } else if (strcasecmp(data.c_str(), "READ") == 0 ||
+                       strcasecmp(data.c_str(), "DEL") == 0) {
+                data += readOrDelMsg();
+                validCmd = true;
+            } else if (strcasecmp(data.c_str(), "HELP") == 0) {
+                help();
+            } else
+                continue;
+
+            // Ignore last newline to empty getline buffer
+            std::cin.ignore(INT_MAX, '\n');
+        }
+
+        // Send Client input data to server
+        if ((send(sock, data.c_str(), data.size() + 1, 0)) == -1) {
+            perror(
+                "An error occurred while trying to send the data to the server.\n");
+            continue;
+        }
+        // Receive answer from server
+        memset(buffer, 0, 4096);
+        int feedback = recv(sock, buffer, 4096, 0);
+
+        if (feedback == -1) {
+            perror("An error occurred while trying to receive the data from the "
+                   "server.\n");
+            break;
+        } else if (feedback == 0) {
+            std::cout << "Server closed remote socket\n";
+            break;
+        } else {
+            std::cout << std::string(buffer, feedback) << std::endl;
+        }
+
+    } while (true);
+
+    close(sock);
+
+    return EXIT_SUCCESS;
+}
+
+std::string sendMsg() {
+    std::string sender, receiver, subject, msg, msgLine;
+
+    std::cout << "\nSender: ";
+    std::cin >> sender;
+    std::cout << "Receiver: ";
+    std::cin >> receiver;
+    std::cout << "Subject: ";
+    std::cin >> subject;
+    std::cout << "Message: ";
+
+    while (msgLine != ".") {
+        std::cin >> msgLine;
+        msg += msgLine + "\n";
+    }
+
+    if (sender.size() > 8)
+        sender.resize(8);
+    if (receiver.size() > 8)
+        receiver.resize(8);
+    if (subject.size() > 80)
+        subject.resize(80);
+
+    return "\n" + sender + "\n" + receiver + "\n" + subject + "\n" + msg + "\n";
+}
+
+std::string listMsg() {
+    std::string username;
+
+    std::cout << "\nUsername: ";
+    std::cin >> username;
+
+    return "\n" + username + "\n";
+}
+
+std::string readOrDelMsg() {
+    std::string username, msgNr;
+    std::cout << "\nUsername: ";
+    std::cin >> username;
+    std::cout << "Message-Number: ";
+    std::cin >> msgNr;
+
+    return "\n" + username + "\n" + msgNr + "\n";
+}
 
 static void usage() {
     std::cout << "Usage Client:\n\t./twmailer-client <ip> <port>\n";
@@ -56,144 +211,4 @@ static void help() {
         }
         break;
     }
-}
-
-int main(int argc, char *argv[]) {
-    struct sockaddr_in server;
-    int port;
-    char buffer[4096];
-
-    if (argc != 3) {
-        usage();
-        exit(1);
-    }
-
-    // Create a client socket
-    int s0 = socket(AF_INET, SOCK_STREAM, 0);
-    if (s0 < 0) {
-        perror("Socket error.");
-        return EXIT_FAILURE;
-    }
-
-    port = atoi(argv[2]);
-
-    // Assign port and address of server
-    memset(&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    // Deprecated
-    inet_aton(argv[1], &server.sin_addr);
-
-    // Connect to server
-    if (connect(s0, (struct sockaddr *)&server, sizeof(server)) == -1) {
-        perror("Connect error - no server available");
-        return EXIT_FAILURE;
-    }
-
-    // Print available commands
-    commands();
-
-    std::string input;
-    // Client Input
-    do {
-        bool validCmd = false;
-        std::string data;
-
-        // Input valid command
-        while (!validCmd) {
-            data = "";
-            std::cout << "> ";
-            getline(std::cin, input);
-            data += input;
-
-            if (data == "QUIT") {
-                return EXIT_SUCCESS;
-            }
-
-            if (data == "SEND") {
-                data += sendMsg();
-                validCmd = true;
-            } else if (data == "LIST") {
-                data += listMsg();
-                validCmd = true;
-            } else if (data == "READ" || data == "DEL") {
-                data += readOrDelMsg();
-                validCmd = true;
-            } else if (data == "HELP") {
-                help();
-            }
-            // Ignore last newline, to empty getline buffer
-            std::cin.ignore(INT_MAX, '\n');
-        }
-
-        // Send Client Input to server
-        if ((send(s0, data.c_str(), data.size() + 1, 0)) == -1) {
-            perror("Input Error");
-            continue;
-        }
-        // Receive answer from server
-        memset(buffer, 0, 4096);
-        int feedback = recv(s0, buffer, 4096, 0);
-
-        if (feedback == -1) {
-            perror("recv error");
-            break;
-        } else if (feedback == 0) {
-            printf("Server closed remote socket\n");
-            break;
-        } else {
-            std::cout << std::string(buffer, feedback) << std::endl;
-        }
-
-    } while (true);
-
-    close(s0);
-
-    std::cout << "Goodbye World from Client!" << std::endl;
-    return 0;
-}
-
-std::string sendMsg() {
-    std::string sender, receiver, subject, msg, msgLine;
-
-    std::cout << "\nSender: ";
-    std::cin >> sender;
-    std::cout << "Receiver: ";
-    std::cin >> receiver;
-    std::cout << "Subject: ";
-    std::cin >> subject;
-    std::cout << "Message: ";
-    while (msgLine != ".") {
-        std::cin >> msgLine;
-        msg += msgLine + "\n";
-    }
-
-    if (sender.size() > 8)
-        receiver.resize(8);
-    if (receiver.size() > 8)
-        receiver.resize(8);
-    if (subject.size() > 80)
-        subject.resize(80);
-
-    return "\n" + sender + "\n" + receiver + "\n" + subject + "\n" + msg + "\n";
-}
-
-std::string listMsg() {
-    std::string username;
-
-    std::cout << "\nUsername: ";
-    std::cin >> username;
-
-    return "\n" + username + "\n";
-}
-
-std::string readOrDelMsg() {
-    std::string username, msgNr;
-
-    std::cout << "\nUsername: ";
-    std::cin >> username;
-    std::cout << "Message-Number: ";
-    std::cin >> msgNr;
-
-    return "\n" + username + "\n" + msgNr + "\n";
 }

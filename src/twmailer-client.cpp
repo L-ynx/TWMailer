7598b5b9b19fb2376989356f32863609bdc2c1b2
L-ynx <raza.gh97@gmail.com>
2022-12-05 20:54:26 +0100

    Refine commands

--- src/twmailer-client.cpp
+++ src/twmailer-client.cpp
@@ -1,10 +1,15 @@
 #include <arpa/inet.h>
+#include <climits>
 #include <iostream>
 #include <netdb.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+std::string sendMsg();
+std::string listMsg();
+std::string readOrDelMsg();
 
 static void usage() {
     std::cout << "Usage Client:\n\t./twmailer-client <ip> <port>\n";
@@ -63,6 +68,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // Create a client socket
     int s0 = socket(AF_INET, SOCK_STREAM, 0);
     if (s0 < 0) {
         perror("Socket error.");
@@ -71,75 +77,61 @@ int main(int argc, char *argv[]) {
 
     port = atoi(argv[2]);
 
+    // Assign port and address of server
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     // Deprecated
     inet_aton(argv[1], &server.sin_addr);
 
+    // Connect to server
     if (connect(s0, (struct sockaddr *)&server, sizeof(server)) == -1) {
         perror("Connect error - no server available");
         return EXIT_FAILURE;
     }
 
+    // Print available commands
     commands();
-    std::string input;
 
+    std::string input;
+    // Client Input
     do {
-        int type = 0;
-        int counter = 1;
+        bool validCmd = false;
         std::string data;
 
-        while (type == 0) {
+        // Input valid command
+        while (!validCmd) {
             data = "";
             std::cout << "> ";
             getline(std::cin, input);
             data += input;
 
-            // Here we could possibly use a function pointer
-            if (data == "SEND")
-                type = 1;
-            else if (data == "LIST")
-                type = 2;
-            else if (data == "READ" || data == "DEL") {
-                type = 2;
-                counter = 2;
-            } else if (data == "HELP")
-                type = 3;
-            else if (data == "QUIT")
-                type = 4;
-
-            data += "\n";
-
-            switch (type) {
-            case 1:
-                while (input != ".") {
-                    getline(std::cin, input);
-                    data += input + "\n";
-                }
-                break;
-
-            case 2:
-                for (int i = 0; i < counter; i++) {
-                    getline(std::cin, input);
-                    data += input + "\n";
-                }
-                break;
-            case 3:
-                help(); // Why does it print the default line, after exiting this
-                        // function?
-                type = 0;
-                break;
-            default:
-                std::cout << "Please enter a valid command." << std::endl;
-                break;
+            if (data == "QUIT") {
+                return EXIT_SUCCESS;
             }
+
+            if (data == "SEND") {
+                data += sendMsg();
+                validCmd = true;
+            } else if (data == "LIST") {
+                data += listMsg();
+                validCmd = true;
+            } else if (data == "READ" || data == "DEL") {
+                data += readOrDelMsg();
+                validCmd = true;
+            } else if (data == "HELP") {
+                help();
+            }
+            // Ignore last newline, to empty getline buffer
+            std::cin.ignore(INT_MAX, '\n');
         }
 
+        // Send Client Input to server
         if ((send(s0, data.c_str(), data.size() + 1, 0)) == -1) {
             perror("Input Error");
             continue;
         }
+        // Receive answer from server
         memset(buffer, 0, 4096);
         int feedback = recv(s0, buffer, 4096, 0);
 
@@ -157,6 +149,51 @@ int main(int argc, char *argv[]) {
 
     close(s0);
 
-    std::cout << "Hello, World from Client!" << std::endl;
+    std::cout << "Goodbye World from Client!" << std::endl;
     return 0;
+}
+
+std::string sendMsg() {
+    std::string sender, receiver, subject, msg, msgLine;
+
+    std::cout << "\nSender: ";
+    std::cin >> sender;
+    std::cout << "Receiver: ";
+    std::cin >> receiver;
+    std::cout << "Subject: ";
+    std::cin >> subject;
+    std::cout << "Message: ";
+    while (msgLine != ".") {
+        std::cin >> msgLine;
+        msg += msgLine + "\n";
+    }
+
+    if (sender.size() > 8)
+        receiver.resize(8);
+    if (receiver.size() > 8)
+        receiver.resize(8);
+    if (subject.size() > 80)
+        subject.resize(80);
+
+    return "\n" + sender + "\n" + receiver + "\n" + subject + "\n" + msg + "\n";
+}
+
+std::string listMsg() {
+    std::string username;
+
+    std::cout << "\nUsername: ";
+    std::cin >> username;
+
+    return "\n" + username + "\n";
+}
+
+std::string readOrDelMsg() {
+    std::string username, msgNr;
+
+    std::cout << "\nUsername: ";
+    std::cin >> username;
+    std::cout << "Message-Number: ";
+    std::cin >> msgNr;
+
+    return "\n" + username + "\n" + msgNr + "\n";
 }

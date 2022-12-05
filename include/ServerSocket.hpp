78622bac37e4a835a4a72d5712fe76679e3beb05
Skyler Mayfield <skyler544@gmail.com>
2022-12-05 20:57:33 +0100

    formatting

--- include/ServerSocket.hpp
+++ include/ServerSocket.hpp
@@ -2,14 +2,14 @@
 #define SERVER_SOCKET_H_
 
 class ServerSocket {
-    private:
-        int port;
-        int serverAddress = -1;
+  private:
+    int port;
+    int serverAddress = -1;
 
-    public:
-        ServerSocket(int port);
+  public:
+    ServerSocket(int port);
 
-        int getServerAddress();
+    int getServerAddress();
 };
 
 #endif // SERVER_SOCKET_H_
